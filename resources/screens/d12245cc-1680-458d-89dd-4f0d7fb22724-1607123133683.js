5060521bafeb041723d38ad99df79d85deabe340
Victor <vconesa@hotmail.com>
2020-12-04 15:11:15 -0800

    Initial commit

---
+++ resources/screens/d12245cc-1680-458d-89dd-4f0d7fb22724-1607123133683.js
@@ -0,0 +1,51 @@
+jQuery("#simulation")
+  .on("mouseenter dragenter", ".s-d12245cc-1680-458d-89dd-4f0d7fb22724 .mouseenter", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getDirectEventFirer(this);
+    if(jFirer.is("#s-Rectangle_1") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeStyle",
+                  "parameter": [ {
+                    "#s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-Rectangle_1": {
+                      "attributes": {
+                        "font-size": "0pt"
+                      }
+                    }
+                  },{
+                    "#s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-Rectangle_1 > .backgroundLayer > .colorLayer": {
+                      "attributes": {
+                        "background-color": "#8B4C4C"
+                      }
+                    }
+                  } ],
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    }
+  })
+  .on("mouseleave dragleave", ".s-d12245cc-1680-458d-89dd-4f0d7fb22724 .mouseleave", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getDirectEventFirer(this);
+    if(jFirer.is("#s-Rectangle_1")) {
+      jEvent.undoCases(jFirer);
+    }
+  });
---
+++ review/screens/d12245cc-1680-458d-89dd-4f0d7fb22724.js
@@ -0,0 +1,45 @@
+var content='<div class="ui-page" deviceName="web" deviceType="desktop" deviceWidth="1280" deviceHeight="800">\
+    <div id="t-f39803f7-df02-4169-93eb-7547fb8c961a" class="template growth-both devWeb canvas firer commentable non-processed" alignment="left" name="Template 1" width="1280" height="800">\
+    <div id="backgroundBox"><div class="colorLayer"></div><div class="imageLayer"></div></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1607123133683.css" />\
+      <!--[if IE]><link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1607123133683-ie.css" /><![endif]-->\
+      <!--[if lte IE 8]><link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1607123133683-ie8.css" /><![endif]-->\
+      <div class="freeLayout">\
+      </div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>\
+\
+    <div id="s-d12245cc-1680-458d-89dd-4f0d7fb22724" class="screen growth-vertical devWeb canvas PORTRAIT firer ie-background commentable non-processed" alignment="left" name="Screen 1" width="1280" height="800">\
+    <div id="backgroundBox"><div class="colorLayer"></div><div class="imageLayer"></div></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/screens/d12245cc-1680-458d-89dd-4f0d7fb22724-1607123133683.css" />\
+      <!--[if IE]><link type="text/css" rel="stylesheet" href="./resources/screens/d12245cc-1680-458d-89dd-4f0d7fb22724-1607123133683-ie.css" /><![endif]-->\
+      <!--[if lte IE 8]><link type="text/css" rel="stylesheet" href="./resources/screens/d12245cc-1680-458d-89dd-4f0d7fb22724-1607123133683-ie8.css" /><![endif]-->\
+      <div class="freeLayout">\
+      <div id="s-Rectangle_1" class="pie rectangle manualfit firer mouseenter mouseleave commentable non-processed" customid="Rectangle 1"   datasizewidth="270.0px" datasizeheight="265.0px" datasizewidthpx="270.0" datasizeheightpx="265.0" dataX="296.0" dataY="145.0" >\
+        <div class="backgroundLayer">\
+          <div class="colorLayer"></div>\
+          <div class="imageLayer"></div>\
+        </div>\
+        <div class="borderLayer">\
+          <div class="paddingLayer">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-Rectangle_1_0"></span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      </div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>\
+  \
+</div>\
+';
+document.getElementById("chromeTransfer").innerHTML = content;
